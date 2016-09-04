a65f3f032f9e0169fe8eb1f0c8900603ecd8db0d
Alex Dai <re757575@gmail.com>
2016-09-04 15:12:35 +0800

    add component lifecycle

--- src/ProfileComponent.js
+++ src/ProfileComponent.js
@@ -10,6 +10,36 @@ export default class ProfileComponent extends Component {
 	}
   }
 
+  componentWillMount() {
+    console.log('componentWillMount');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('shouldComponentUpdate');
+	// return true;
+	return nextState.address !== this.state.address;
+  }
+
+  componentWillUpdate(inextProps, nextState) {
+    console.log('componentWillUpdate');
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
   handleChangeAddress(event) {
     this.setState({
 	  address: event.target.value
@@ -20,6 +50,8 @@ export default class ProfileComponent extends Component {
   }
 
   render() {
+
+   console.log('render');
 
    var address = this.state.address;
 
