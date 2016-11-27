c77a6a8b3395e15e301448e436a2248a4e7e0e45
Alex Dai <re757575@gmail.com>
2016-11-27 15:55:14 +0800

    clear project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react';
-import HelloWorld, { HelloWorld2 } from './HelloWorldComponent';
-import Profile from './ProfileComponent';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
-
-	this.state = {
-	  appTitle: 'Simple React'
-	};
+    this.state = {
+      appTitle: 'Simple React'
+    };
   }
-
-  handleChangeTitle(title) {
-    this.setState({
-	  appTitle: title
-	});
-  }
-
 
   render() {
     return (
-	 <div>
-	   <h1>{this.state.appTitle}</h1>
-	   <HelloWorld />
-       <HelloWorld2 />
-	   <Profile name="alex" age="26" changeTitle={this.handleChangeTitle.bind(this)}/>
-	 </div>
+      <div>
+        <h1>{this.state.appTitle}</h1>
+      </div>
     );
   }
 }
