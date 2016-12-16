19eb49663a5e82bd140e8ca601b5c50433c9f231
Alex.Dai <alex.dai@fortune-inc.com>
2016-12-16 17:17:23 +0800

    新增 redux-logger

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger';
 import rootReducer from './reducers'
 import App from './App';
 
-let store = createStore(rootReducer, applyMiddleware(thunk));
+const middleware = [ thunk ];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+let store = createStore(
+  rootReducer,
+  applyMiddleware(...middleware)
+);
 
 ReactDOM.render(
   <Provider store={store}>
