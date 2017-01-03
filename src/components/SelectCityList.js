7b448397a422ad95eb804543a5368acda98429af
Alex Dai <re757575@gmail.com>
2017-01-03 19:36:11 +0800

    修正 select width

--- src/components/SelectCityList.js
+++ src/components/SelectCityList.js
@@ -7,7 +7,8 @@ import cityIdList, { getCityNameById } from '../constants/cityIdList';
 const style = {
   float: 'left',
   marginRight: '30px',
-  marginTop: '20px'
+  marginTop: '20px',
+  width: 200,
 }
 
 const options = cityIdList.map((v, k) =>
